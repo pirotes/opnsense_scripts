42f2e129692c598f0d2c3c02781d9a8c481b9315
H.J.Yang <dino3724@outlook.com>
2025-12-22 13:35:07 +0900

    Create configureopnsense.sh

---
+++ scripts/configureopnsense.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+# Script Params
+# $1 = OPNScriptURI
+# $2 = OpnVersion
+# $3 = active_active_primary/active_active_secondary/single
+# $4 = Trusted Nic subnet GW IP
+# $5 = ELB VIP Address
+# $6 = Private IP Secondary Server
+
+# Check if Primary or Secondary Server to setup Firewal Sync
+# Note: Firewall Sync should only be setup in the Primary Server
+if [ "$3" = "active_active_primary" ]; then
+    fetch $1config-active-active-primary.xml
+    sed -i "" "s/yyy.yyy.yyy.yyy/$4/" config-active-active-primary.xml
+    sed -i "" "s/www.www.www.www/$5/" config-active-active-primary.xml
+    sed -i "" "s/xxx.xxx.xxx.xxx/$6/" config-active-active-primary.xml
+    sed -i "" "s/<hostname>OPNsense<\/hostname>/<hostname>OPNsense-Primary<\/hostname>/" config-active-active-primary.xml
+    cp config-active-active-primary.xml /usr/local/etc/config.xml
+elif [ "$3" = "active_active_secondary" ]; then
+    fetch $1config-active-active-secondary.xml
+    sed -i "" "s/yyy.yyy.yyy.yyy/$4/" config-active-active-secondary.xml
+    sed -i "" "s/www.www.www.www/$5/" config-active-active-secondary.xml
+    sed -i "" "s/<hostname>OPNsense<\/hostname>/<hostname>OPNsense-Secondary<\/hostname>/" config-active-active-secondary.xml
+    cp config-active-active-secondary.xml /usr/local/etc/config.xml
+elif [ "$3" = "single" ]; then
+    fetch $1config.xml
+    sed -i "" "s/yyy.yyy.yyy.yyy/$4/" config.xml
+    cp config.xml /usr/local/etc/config.xml
+fi
+
+#OPNSense default configuration template
+#fetch https://raw.githubusercontent.com/dmauser/opnazure/dev_active_active/scripts/$1
+#fetch https://raw.githubusercontent.com/dmauser/opnazure/master/scripts/$1
+#cp $1 /usr/local/etc/config.xml
+
+# 1. Package to get root certificate bundle from the Mozilla Project (FreeBSD)
+# 2. Install bash to support Azure Backup integration
+#env IGNORE_OSVERSION=yes
+#pkg bootstrap -f; pkg update -f
+#env ASSUME_ALWAYS_YES=YES pkg install ca_root_nss && pkg install -y bash
+
+#Download OPNSense Bootstrap and Permit Root Remote Login
+#fetch https://raw.githubusercontent.com/opnsense/update/master/src/bootstrap/opnsense-bootstrap.sh.in
+#fetch https://raw.githubusercontent.com/opnsense/update/7ba940e0d57ece480540c4fd79e9d99a87f222c8/src/bootstrap/opnsense-bootstrap.sh.in
+fetch https://raw.githubusercontent.com/opnsense/update/master/src/bootstrap/opnsense-bootstrap.sh.in
+sed -i "" 's/#PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+#OPNSense
+# Due to a recent change in pkg the following commands no longer finish with status code 0
+#		pkg unlock -a
+#		pkg delete -fa
+# This resplace of set -e which force the script to finish in case of non status code 0 has to be inplace
+sed -i "" "s/set -e/#set -e/g" opnsense-bootstrap.sh.in
+sed -i "" "s/reboot/shutdown -r +1/g" opnsense-bootstrap.sh.in
+sh ./opnsense-bootstrap.sh.in -y -r "$2"
+
+# WAagent
+# https://forum.opnsense.org/index.php?topic=40291.msg197657#msg197657
+pkg install azure-agent
+echo 'waagent_enable="YES"' >> /etc/rc.conf
+
+# Installing bash - This is a requirement for Azure custom Script extension to run
+pkg install -y bash
+pkg install -y os-frr
+
+# Remove wrong route at initialization
+cat > /usr/local/etc/rc.syshook.d/start/22-remoteroute <<EOL
+#!/bin/sh
+route delete 168.63.129.16
+EOL
+chmod +x /usr/local/etc/rc.syshook.d/start/22-remoteroute
+
+#Adds support to LB probe from IP 168.63.129.16
+#Add Azure VIP on Arp table
+echo # Add Azure Internal VIP >> /etc/rc.conf
+echo static_arp_pairs=\"azvip\" >>  /etc/rc.conf
+echo static_arp_azvip=\"168.63.129.16 12:34:56:78:9a:bc\" >> /etc/rc.conf
+# Makes arp effective
+service static_arp start
+# To survive boots adding to OPNsense Autorun/Bootup:
+echo service static_arp start >> /usr/local/etc/rc.syshook.d/start/20-freebsd
+
+# Reset WebGUI certificate
+echo #\!/bin/sh >> /usr/local/etc/rc.syshook.d/start/94-restartwebgui
+echo configctl webgui restart renew >> /usr/local/etc/rc.syshook.d/start/94-restartwebgui
+echo rm /usr/local/etc/rc.syshook.d/start/94-restartwebgui >> /usr/local/etc/rc.syshook.d/start/94-restartwebgui
+chmod +x /usr/local/etc/rc.syshook.d/start/94-restartwebgui
