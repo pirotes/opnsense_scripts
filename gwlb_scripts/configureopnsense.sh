485e6ace337f05801b89eba5007aa51d0bb68113
H.J.Yang <dino3724@outlook.com>
2026-01-04 03:34:36 +0900

    Create configureopnsense.sh

---
+++ gwlb_scripts/configureopnsense.sh
@@ -0,0 +1,220 @@
+#!/bin/sh
+
+# Script Params
+# $1 = OPNScriptURI
+# $2 = OpnVersion
+# $3 = active_active_primary/active_active_secondary/single
+# $4 = Trusted Nic subnet GW IP
+# $5 = Peer Server IP - Private IP Primary or Secondary Server
+# $6 = vxlan local ip - vm trusted nic ip
+# $7 = vxlan remote ip - gwlb frontend ip
+# $8 = vxlan internal local port - 10800
+# $9 = vxlan external local port - 10801
+# $10 = vxlan internal identifier - 800 (800~1000)
+# $11 = vxlan external identifier - 801 (800~1000)
+
+# ----------------------------
+# Helpers
+# ----------------------------
+die() { echo "[ERR] $*" >&2; exit 1; }
+
+# Check if Primary or Secondary Server to setup Firewal Sync
+# Note: Firewall Sync should only be setup in the Primary Server
+if [ "$3" = "active_active_primary" ]; then
+    fetch $1gwlb-config-active-active-primary.xml
+    sed -i "" "s/yyy.yyy.yyy.yyy/$4/" gwlb-config-active-active-primary.xml
+    sed -i "" "s/xxx.xxx.xxx.xxx/$5/" gwlb-config-active-active-primary.xml
+    sed -i "" "s/lll.lll.lll.lll/$6/" gwlb-config-active-active-primary.xml
+    sed -i "" "s/rrr.rrr.rrr.rrr/$7/" gwlb-config-active-active-primary.xml
+    sed -i "" "s/zzz/${10}/" gwlb-config-active-active-primary.xml
+    sed -i "" "s/eeee/$8/" gwlb-config-active-active-primary.xml
+    sed -i "" "s/ccc/${11}/" gwlb-config-active-active-primary.xml
+    sed -i "" "s/dddd/$9/" gwlb-config-active-active-primary.xml
+    sed -i "" "s/<hostname>OPNsense<\/hostname>/<hostname>OPNsense-Primary<\/hostname>/" gwlb-config-active-active-primary.xml
+    cp gwlb-config-active-active-primary.xml /usr/local/etc/config.xml
+elif [ "$3" = "active_active_secondary" ]; then
+    fetch $1gwlb-config-active-active-secondary.xml
+    sed -i "" "s/yyy.yyy.yyy.yyy/$4/" gwlb-config-active-active-secondary.xml
+    sed -i "" "s/xxx.xxx.xxx.xxx/$5/" gwlb-config-active-active-secondary.xml
+    sed -i "" "s/lll.lll.lll.lll/$6/" gwlb-config-active-active-secondary.xml
+    sed -i "" "s/rrr.rrr.rrr.rrr/$7/" gwlb-config-active-active-secondary.xml
+    sed -i "" "s/zzz/${10}/" gwlb-config-active-active-secondary.xml
+    sed -i "" "s/eeee/$8/" gwlb-config-active-active-secondary.xml
+    sed -i "" "s/ccc/${11}/" gwlb-config-active-active-secondary.xml
+    sed -i "" "s/dddd/$9/" gwlb-config-active-active-secondary.xml
+    sed -i "" "s/<hostname>OPNsense<\/hostname>/<hostname>OPNsense-Secondary<\/hostname>/" gwlb-config-active-active-secondary.xml
+    cp gwlb-config-active-active-secondary.xml /usr/local/etc/config.xml
+elif [ "$3" = "single" ]; then
+    fetch $1config.xml
+    sed -i "" "s/yyy.yyy.yyy.yyy/$4/" config.xml
+    sed -i "" "s/lll.lll.lll.lll/$6/" config.xml
+    sed -i "" "s/rrr.rrr.rrr.rrr/$7/" config.xml
+    sed -i "" "s/zzz/${10}/" config.xml
+    sed -i "" "s/eeee/$8/" config.xml
+    sed -i "" "s/ccc/${11}/" config.xml
+    sed -i "" "s/dddd/$9/" config.xml
+    cp config.xml /usr/local/etc/config.xml
+else
+    die "Unknown mode: $3"
+fi
+
+#OPNSense default configuration template
+#fetch https://raw.githubusercontent.com/dmauser/opnazure/dev_active_active/scripts/$1
+#fetch https://raw.githubusercontent.com/dmauser/opnazure/master/scripts/$1
+#cp $1 /usr/local/etc/config.xml
+
+# 1. Package to get root certificate bundle from the Mozilla Project (FreeBSD)
+# 2. Install bash to support Azure Backup integration
+#env IGNORE_OSVERSION=yes
+#pkg bootstrap -f; pkg update -f
+#env ASSUME_ALWAYS_YES=YES pkg install ca_root_nss && pkg install -y bash
+
+#Download OPNSense Bootstrap and Permit Root Remote Login
+#fetch https://raw.githubusercontent.com/opnsense/update/master/src/bootstrap/opnsense-bootstrap.sh.in
+#fetch https://raw.githubusercontent.com/opnsense/update/7ba940e0d57ece480540c4fd79e9d99a87f222c8/src/bootstrap/opnsense-bootstrap.sh.in
+fetch https://raw.githubusercontent.com/opnsense/update/master/src/bootstrap/opnsense-bootstrap.sh.in || die "fetch bootstrap failed"
+sed -i "" 's/#PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+#OPNSense
+# Due to a recent change in pkg the following commands no longer finish with status code 0
+#		pkg unlock -a
+#		pkg delete -fa
+# This resplace of set -e which force the script to finish in case of non status code 0 has to be inplace
+sed -i "" "s/set -e/#set -e/g" opnsense-bootstrap.sh.in
+sed -i "" "s/reboot/shutdown -r +1/g" opnsense-bootstrap.sh.in
+sh ./opnsense-bootstrap.sh.in -y -r "$2" || die "bootstrap failed"
+
+# WAagent
+# https://forum.opnsense.org/index.php?topic=40291.msg197657#msg197657
+pkg install -y azure-agent
+# echo 'waagent_enable="YES"' >> /etc/rc.conf
+
+# Fix waagent by replacing configuration settings
+pyver=$(python3 -V | awk '{print $2}' | cut -d. -f1,2)
+ln -s /usr/local/bin/python${pyver} /usr/local/bin/python
+##sed -i "" 's/command_interpreter="python"/command_interpreter="python3"/' /etc/rc.d/waagent
+##sed -i "" 's/#!\/usr\/bin\/env python/#!\/usr\/bin\/env python3/' /usr/local/sbin/waagent
+# sed -i "" 's/ResourceDisk.EnableSwap=y/ResourceDisk.EnableSwap=n/' /etc/waagent.conf
+fetch $1actions_waagent.conf || die "fetch actions_waagent.conf failed"
+cp actions_waagent.conf /usr/local/opnsense/service/conf/actions.d
+
+# Installing bash - This is a requirement for Azure custom Script extension to run
+pkg install -y bash
+pkg install -y os-frr
+
+# Remove wrong route at initialization
+cat > /usr/local/etc/rc.syshook.d/start/22-remoteroute <<EOL
+#!/bin/sh
+route delete 168.63.129.16
+EOL
+chmod +x /usr/local/etc/rc.syshook.d/start/22-remoteroute
+
+# 혹시 이전 배포에서 남아있는 25-azure가 있으면 제거 (중복/조기 적용 방지)
+rm -f /usr/local/etc/rc.syshook.d/start/25-azure
+
+# 안전 기본 MTU: GWLB/VXLAN + FreeBSD 기준으로 3800 권장 (3900은 경계값일 수 있음)
+MTU_SAFE=3800
+
+# mode가 active_active_* 또는 single 이더라도,
+# vxlan을 쓴다면 여기서 늦게 구성. (vxlan0/1 존재 여부는 ifconfig로 확인)
+cat > /usr/local/etc/rc.syshook.d/start/99-azure-net <<EOL
+#!/bin/sh
+# Post-boot network adjustments for Azure GWLB/VXLAN stability
+# Generated by provisioning script.
+
+MTU=${MTU_SAFE}
+
+# Wait a bit for interfaces/services to settle (avoids deadlock during boot)
+for i in 1 2 3 4 5 6 7 8 9 10; do
+  if ifconfig hn0 >/dev/null 2>&1 && ifconfig hn1 >/dev/null 2>&1; then
+    break
+  fi
+  sleep 1
+done
+
+# 1) Set MTU on hn* (OPNsense logical ifaces)
+ifconfig hn0 mtu \${MTU} >/dev/null 2>&1 || true
+ifconfig hn1 mtu \${MTU} >/dev/null 2>&1 || true
+
+# 2) If MANA ifaces exist, align MTU as well (best-effort insurance)
+if ifconfig mana0 >/dev/null 2>&1; then ifconfig mana0 mtu \${MTU} >/dev/null 2>&1 || true; fi
+if ifconfig mana1 >/dev/null 2>&1; then ifconfig mana1 mtu \${MTU} >/dev/null 2>&1 || true; fi
+
+# 3) Defensive: disable offloads that can interact badly with overlay paths (best-effort)
+# ifconfig hn0 -tso -lro -rxcsum -txcsum >/dev/null 2>&1 || true
+# ifconfig hn1 -tso -lro -rxcsum -txcsum >/dev/null 2>&1 || true
+# if ifconfig mana0 >/dev/null 2>&1; then ifconfig mana0 -tso -lro -rxcsum -txcsum >/dev/null 2>&1 || true; fi
+# if ifconfig mana1 >/dev/null 2>&1; then ifconfig mana1 -tso -lro -rxcsum -txcsum >/dev/null 2>&1 || true; fi
+
+# 4) VXLAN re-config (only if vxlan0/vxlan1 exist)
+#    local=${6}, remote=${7}, ports: internal=${8}, external=${9}
+#    NOTE: identifiers(${10}/${11}) are set in config.xml; here we only ensure endpoints/ports.
+VXLAN_LOCAL="${6}"
+VXLAN_REMOTE="${7}"
+P_INT="${8}"
+P_EXT="${9}"
+
+if ifconfig vxlan0 >/dev/null 2>&1; then
+  ifconfig vxlan0 down >/dev/null 2>&1 || true
+  ifconfig vxlan0 vxlanlocal "\${VXLAN_LOCAL}" vxlanremote "\${VXLAN_REMOTE}" vxlanlocalport "\${P_EXT}" vxlanremoteport "\${P_EXT}" >/dev/null 2>&1 || true
+  ifconfig vxlan0 up >/dev/null 2>&1 || true
+fi
+
+if ifconfig vxlan1 >/dev/null 2>&1; then
+  ifconfig vxlan1 down >/dev/null 2>&1 || true
+  ifconfig vxlan1 vxlanlocal "\${VXLAN_LOCAL}" vxlanremote "\${VXLAN_REMOTE}" vxlanlocalport "\${P_INT}" vxlanremoteport "\${P_INT}" >/dev/null 2>&1 || true
+  ifconfig vxlan1 up >/dev/null 2>&1 || true
+fi
+
+# 5) Bridge membership (only if bridge0 exists)
+if ifconfig bridge0 >/dev/null 2>&1; then
+  if ifconfig vxlan0 >/dev/null 2>&1; then ifconfig bridge0 addm vxlan0 >/dev/null 2>&1 || true; fi
+  if ifconfig vxlan1 >/dev/null 2>&1; then ifconfig bridge0 addm vxlan1 >/dev/null 2>&1 || true; fi
+fi
+
+exit 0
+EOL
+chmod +x /usr/local/etc/rc.syshook.d/start/99-azure-net
+
+#VXLAN config
+# if [ "$3" = "active_active_primary" ]; then
+#     echo ifconfig hn0 mtu 3900 >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig hn1 mtu 3900 >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig vxlan0 down >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig vxlan0 vxlanlocal $6 vxlanremote $7 vxlanlocalport $9 vxlanremoteport $9 >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig vxlan0 up >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig vxlan1 down >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig vxlan1 vxlanlocal $6 vxlanremote $7 vxlanlocalport $8 vxlanremoteport $8 >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig vxlan1 up >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig bridge0 addm vxlan0 >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig bridge0 addm vxlan1 >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     chmod +x /usr/local/etc/rc.syshook.d/start/25-azure 
+# elif [ "$3" = "active_active_secondary" ]; then
+#     echo ifconfig hn0 mtu 3900 >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig hn1 mtu 3900 >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig vxlan0 down >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig vxlan0 vxlanlocal $6 vxlanremote $7 vxlanlocalport $9 vxlanremoteport $9 >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig vxlan0 up >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig vxlan1 down >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig vxlan1 vxlanlocal $6 vxlanremote $7 vxlanlocalport $8 vxlanremoteport $8 >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig vxlan1 up >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig bridge0 addm vxlan0 >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     echo ifconfig bridge0 addm vxlan1 >> /usr/local/etc/rc.syshook.d/start/25-azure
+#     chmod +x /usr/local/etc/rc.syshook.d/start/25-azure
+# fi
+
+#Adds support to LB probe from IP 168.63.129.16
+#Add Azure VIP on Arp table
+echo # Add Azure Internal VIP >> /etc/rc.conf
+echo static_arp_pairs=\"azvip\" >>  /etc/rc.conf
+echo static_arp_azvip=\"168.63.129.16 12:34:56:78:9a:bc\" >> /etc/rc.conf
+# Makes arp effective
+service static_arp start
+# To survive boots adding to OPNsense Autorun/Bootup:
+echo service static_arp start >> /usr/local/etc/rc.syshook.d/start/20-freebsd
+
+# Reset WebGUI certificate
+echo #\!/bin/sh >> /usr/local/etc/rc.syshook.d/start/94-restartwebgui
+echo configctl webgui restart renew >> /usr/local/etc/rc.syshook.d/start/94-restartwebgui
+echo rm /usr/local/etc/rc.syshook.d/start/94-restartwebgui >> /usr/local/etc/rc.syshook.d/start/94-restartwebgui
+chmod +x /usr/local/etc/rc.syshook.d/start/94-restartwebgui
