881125d1c8fa8f5ae267326b39424f6080afa2e6
H.J.Yang <dino3724@outlook.com>
2026-01-09 15:42:04 +0900

    Update configureopnsense.sh

--- gwlb_scripts/configureopnsense.sh
+++ gwlb_scripts/configureopnsense.sh
@@ -76,11 +76,11 @@ pkg install -y bash
 pkg install -y os-frr
 
 # Remove wrong route at initialization
-cat > /usr/local/etc/rc.syshook.d/start/22-remoteroute <<EOL
-#!/bin/sh
-route delete 168.63.129.16
-EOL
-chmod +x /usr/local/etc/rc.syshook.d/start/22-remoteroute
+# cat > /usr/local/etc/rc.syshook.d/start/22-remoteroute <<EOL
+# #!/bin/sh
+# route delete 168.63.129.16
+# EOL
+# chmod +x /usr/local/etc/rc.syshook.d/start/22-remoteroute
 
 #VXLAN config
 if [ "$3" = "active_active_primary" ]; then
@@ -109,15 +109,15 @@ elif [ "$3" = "active_active_secondary" ]; then
     chmod +x /usr/local/etc/rc.syshook.d/start/25-azure
 fi
 
-#Adds support to LB probe from IP 168.63.129.16
-#Add Azure VIP on Arp table
-echo # Add Azure Internal VIP >> /etc/rc.conf
-echo static_arp_pairs=\"azvip\" >>  /etc/rc.conf
-echo static_arp_azvip=\"168.63.129.16 12:34:56:78:9a:bc\" >> /etc/rc.conf
-# Makes arp effective
-service static_arp start
-# To survive boots adding to OPNsense Autorun/Bootup:
-echo service static_arp start >> /usr/local/etc/rc.syshook.d/start/20-freebsd
+# #Adds support to LB probe from IP 168.63.129.16
+# #Add Azure VIP on Arp table
+# echo # Add Azure Internal VIP >> /etc/rc.conf
+# echo static_arp_pairs=\"azvip\" >>  /etc/rc.conf
+# echo static_arp_azvip=\"168.63.129.16 12:34:56:78:9a:bc\" >> /etc/rc.conf
+# # Makes arp effective
+# service static_arp start
+# # To survive boots adding to OPNsense Autorun/Bootup:
+# echo service static_arp start >> /usr/local/etc/rc.syshook.d/start/20-freebsd
 
 # Reset WebGUI certificate
 echo #\!/bin/sh >> /usr/local/etc/rc.syshook.d/start/94-restartwebgui
